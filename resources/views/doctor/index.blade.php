9555f68de831ab3a6d187003d5b24cd99b7e9358
zhaoyang <602155776@qq.com>
2018-01-26 15:57:51 +0800

    渣打h5

---
+++ resources/views/doctor/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh_cn">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+    <table>
+        <tr>
+
+        </tr>
+    </table>
+</body>
+</html>
