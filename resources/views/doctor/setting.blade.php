0f2f21189a2cdf0605eb2216f61c2ca2ae2fc9ed
zhaoyang <602155776@qq.com>
2018-04-01 15:08:08 +0800

    门诊系统

--- resources/views/doctor/setting.blade.php
+++ resources/views/doctor/setting.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
     <div class="container">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
         <div class="row">
             <div class="col-md-4">
                 <div class="panel panel-primary">
